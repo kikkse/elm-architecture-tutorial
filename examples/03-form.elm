e97db2b8cc139aad8e5339fbc4f3c222798ce46b
Kristine Grina <grina.kristine@gmail.com>
2017-07-25 22:45:54 +0300

    Started working with form validation

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,14 +1,16 @@
-import Html exposing (..)
+module Main exposing (..)
+
+import Html exposing (Html, div, input, button, text, br)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 
 
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
 
 
 
@@ -16,15 +18,16 @@ main =
 
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  }
+    { name : String
+    , password : String
+    , passwordAgain : String
+    , age : Int
+    }
 
 
 model : Model
 model =
-  Model "" "" ""
+    Model "" "" "" -1
 
 
 
@@ -35,19 +38,27 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Age String
+    | Validate
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
+    case msg of
+        Name name ->
+            { model | name = name }
 
-    Password password ->
-      { model | password = password }
+        Password password ->
+            { model | password = password }
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
+        PasswordAgain password ->
+            { model | passwordAgain = password }
+
+        Age ageEntered ->
+            { model | age = Result.withDefault -1 (String.toInt ageEntered) }
+
+        Validate ->
+            Debug.crash "Validate button hasn't been implemented"
 
 
 
@@ -56,21 +67,24 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ type_ "text", placeholder "Name", onInput Name ] []
-    , input [ type_ "password", placeholder "Password", onInput Password ] []
-    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , viewValidation model
-    ]
+    div []
+        [ input [ type_ "text", placeholder "Name", onInput Name ] []
+        , input [ type_ "password", placeholder "Password", onInput Password ] []
+        , br [] []
+        , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+        , input [ type_ "number", placeholder "Age", onInput Age ] []
+        , button [ onClick Validate ] [ text "VALIDATE!!!" ]
+        , viewValidation model
+        ]
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
-  in
-    div [ style [("color", color)] ] [ text message ]
+    let
+        ( color, message ) =
+            if model.password == model.passwordAgain then
+                ( "green", "OK" )
+            else
+                ( "red", "Passwords do not match!" )
+    in
+        div [ style [ ( "color", color ) ] ] [ text message ]
